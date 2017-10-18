33aa0cf78be2d5a831a464d94c64810314a5535c
natysantoss <31621136+natysantoss@users.noreply.github.com>
2017-10-18 15:35:27 -0200

    Tela Model

---
+++ app/src/main/java/br/edu/unileao/syteplus/UsuarioController.java
@@ -0,0 +1,14 @@
+package br.edu.unileao.syteplus;
+
+/**
+ * Created by Naty Santos on 16/10/2017.
+ */
+
+class UsuarioController {
+    public void renoDados(AtualizarDados atualizarDados) {
+
+    }
+
+    public void pegarDados(AtualizarDados atualizarDados) {
+    }
+}
